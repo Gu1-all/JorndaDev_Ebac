d762e9696f5edcd8ff59899098f9573efaae246a
Guilherme Pinheiro <guilhermepinheiro.all@gmail.com>
2023-03-29 13:13:33 -0300

    Código Base

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World!
+        </p>
+        <a
+          className="App-link"
+          href="https://www.instagram.com/gu1.al/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @gu1.al
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
