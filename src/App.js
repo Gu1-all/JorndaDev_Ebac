63b84324032e972da052c8dba9fd758c51fd545f
Guilherme Pinheiro <guilhermepinheiro.all@gmail.com>
2023-03-30 17:06:50 -0300

    aula 02

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Video from './pages/Video';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://www.instagram.com/gu1.al/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          @gu1.al
-        </a>
-      </header>
+      <div className='app__videos'> 
+        <Video />
+        <Video />
+        <Video />
+      </div>
     </div>
   );
 }
